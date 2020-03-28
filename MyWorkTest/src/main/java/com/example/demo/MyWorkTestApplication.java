fd06648afaae620aebcf1507b3b96693d3d5163e
Abhijeet Singh <abhijeet.raj2417@gmail.com>
2020-03-28 17:42:27 +0530

    TestCommmit

---
+++ MyWorkTest/src/main/java/com/example/demo/MyWorkTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorkTestApplication.class, args);
+	}
+
+}
