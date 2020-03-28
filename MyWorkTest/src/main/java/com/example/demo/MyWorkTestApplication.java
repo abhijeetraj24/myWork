9860261081a7053fc05bc8bf86800d5f5c0f854b
Abhijeet Singh <abhijeet.raj2417@gmail.com>
2020-03-28 17:44:11 +0530

    Change In Main Method

--- MyWorkTest/src/main/java/com/example/demo/MyWorkTestApplication.java
+++ MyWorkTest/src/main/java/com/example/demo/MyWorkTestApplication.java
@@ -8,6 +8,8 @@ public class MyWorkTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyWorkTestApplication.class, args);
+		
+		System.out.println("  TESTING GIT LAB ");
 	}
 
 }
